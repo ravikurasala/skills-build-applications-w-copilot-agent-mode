1a3506a43cd9167b620be3f19ea500f3aee69b39
ravikurasala <venkatravi68@gmail.com>
2025-04-08 10:20:36 +0000

    Commit changes

---
+++ frontend/src/components/Teams.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+
+function Teams() {
+  const [teams, setTeams] = useState([]);
+
+  useEffect(() => {
+    fetch('https://miniature-parakeet-5vr54x94x66f7pqg-8000.app.github.dev/api/teams/')
+      .then(response => response.json())
+      .then(data => setTeams(data))
+      .catch(error => console.error('Error fetching teams:', error));
+  }, []);
+
+  return (
+    <div className="container mt-4">
+      <h1 className="text-center text-primary">Teams</h1>
+      <table className="table table-striped table-bordered mt-4">
+        <thead className="table-dark">
+          <tr>
+            <th>Team Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {teams.map(team => (
+            <tr key={team._id}>
+              <td>{team.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Teams;
